43196a6854dd57cbea092d98ef073e830edc1e2a
Mads Kristensen <post@madskristensen.net>
2017-05-04 14:01:38 -0700

    Add project files.

---
+++ src/Commands/AddTemplate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace TemplateCreator
+{
+    internal sealed class AddTemplate
+    {
+        private readonly Package _package;
+
+        private AddTemplate(Package package)
+        {
+            _package = package;
+
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var cmdId = new CommandID(PackageGuids.guidPackageCmdSet, PackageIds.AddTemplate);
+                var cmd = new MenuCommand(Execute, cmdId);
+                commandService.AddCommand(cmd);
+            }
+        }
+
+        public static AddTemplate Instance
+        {
+            get; private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get { return _package; }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new AddTemplate(package);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "AddTemplate";
+
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ src/TemplateCreatorPackage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace TemplateCreator
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [Guid(PackageGuids.guidPackageString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class TemplateCreatorPackage : Package
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+            AddTemplate.Initialize(this);
+        }
+    }
+}
