c24b4523d99ed2107ea7faedc7285293c89119c3
Mads Kristensen <post@madskristensen.net>
2018-09-07 15:24:09 -0700

    Converted to meta package

--- src/Shared/VsHelpers.cs
+++ src/Shared/VsHelpers.cs
@@ -62,7 +62,7 @@ namespace TemplateCreator
             if (project == null)
                 return null;
 
-            if (project.IsKind(ProjectKinds.vsProjectKindSolutionFolder))
+            if (project.IsKind("{66A26720-8FB5-11D2-AA7E-00C04F688DDE}"))//ProjectKinds.vsProjectKindSolutionFolder))
                 return Path.GetDirectoryName(DTE.Solution.FullName);
 
             if (string.IsNullOrEmpty(project.FullName))
